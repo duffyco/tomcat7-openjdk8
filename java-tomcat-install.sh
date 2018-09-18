874622392661eaa97d7140b4956d510974a3e60b
John Duffy <jduffy@cbnco.com>
2018-09-17 20:34:46 -0400

    first commit

---
+++ java-tomcat-install.sh
@@ -0,0 +1,15 @@
+# Install Java
+sudo add-apt-repository -y ppa:openjdk-r/ppa
+
+sudo apt-get -y update
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get -y update --fix-missing
+sudo apt-get install -y openjdk-8-jdk
+
+# Install tomcat
+sudo apt-get install -y  $2
+
+if netstat -tulpen | grep 8080
+then
+	exit 0
+fi
